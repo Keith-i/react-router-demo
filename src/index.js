36f9bccbfc2fd740cd95904531eb79e26b627a62
Keith <754781293@qq.com>
2020-06-08 15:01:32 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import { createStore } from 'redux'
-// import counter from './reducers/counter'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import counter from './reducers/counter'
 
 // // 创建store仓库
-// const store = createStore(counter)
+const store = createStore(counter)
 // store.subscribe(() => {
 //   console.log('state:', store.getState())
 // })
 
 ReactDOM.render(
-  <App />,
+  <Provider store={ store }>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
--- src/view/reduxTest.jsx
+++ src/view/reduxTest.jsx
@@ -1,36 +1,49 @@
 import React, { Component } from 'react'
-import { createStore } from 'redux'
-import counter from '../reducers/counter'
+// import { createStore } from 'redux'
+// import counter from '../reducers/counter'
+import { connect } from 'react-redux'
+import { increment, decrement } from '../action/counter'
 
 // 创建store仓库
-const store = createStore(counter)
+// const store = createStore(counter)
+// store.subscribe(() => {
+//     console.log('state:', store.getState())
+// })
 
-// class reduxTest extends Component {
-
-//   render() {
-//     return (
-//       <div>
-//         reduxTest{ store.getState() }
-//         <button onClick={ () => store.dispatch({ type: 'INCREMENT' }) }>增加</button>
-//         <button onClick={ () => store.dispatch({ type: 'DECREMENT' }) }>减少</button>
-//       </div>
-//     )
-//   }
+class reduxTest extends Component {
+  render() {
+    console.log(this.props)
+    const {increment, decrement} = this.props
+    return (
+      <div>
+        reduxTest{this.props.counter}
+        <button onClick={ () => increment() }>增加</button>
+        <button onClick={ () => decrement() }>减少</button>
+      </div>
+    )
+  }
+}
+// function reduxTest() {
+//   return (
+//     <div>
+//       reduxTest{ store.getState() }
+//       <button onClick={ () => store.dispatch({ type: 'INCREMENT' }) }>增加</button>
+//       <button onClick={ () => store.dispatch({ type: 'DECREMENT' }) }>减少</button>
+//     </div>
+//   )
 // }
-function reduxTest() {
-  return (
-    <div>
-      reduxTest{ store.getState() }
-      <button onClick={ () => store.dispatch({ type: 'INCREMENT' }) }>增加</button>
-      <button onClick={ () => store.dispatch({ type: 'DECREMENT' }) }>减少</button>
-    </div>
-  )
+
+const mapStateToProps = (state) => {
+  return {
+    counter: state
+  }
 }
 
-export default reduxTest
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increment: () => { dispatch(increment()) },
+    decrement: () => { dispatch(decrement()) }
+  }
+}
 
-// let bb = new reduxTest()
-store.subscribe(() => {
-    console.log('state:', store.getState())
-    reduxTest()
-})
+export default connect(mapStateToProps, mapDispatchToProps)(reduxTest)
