1fdd2276e4c4c68c47a82ae6109f2c1b1c132ef3
Keith <754781293@qq.com>
2020-06-08 10:24:16 +0800

    提交

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux'
+import counter from './reducers/counter'
+
+// 创建store仓库
+const store = createStore(counter)
 
 ReactDOM.render(
   <App />,
---
+++ src/reducers/counter.js
@@ -0,0 +1,8 @@
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    default:
+        return state
+  }
+}
+
+export default counter
